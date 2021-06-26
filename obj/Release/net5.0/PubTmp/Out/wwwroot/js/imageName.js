b0fdb8cab70366024e27c68984270f2a1ed142f7
Mariam Soliman <60881649+msa01505@users.noreply.github.com>
2021-06-27 01:53:32 +0200

    Delete obj directory

--- obj/Release/net5.0/PubTmp/Out/wwwroot/js/imageName.js
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path="../lib/jquery/dist/jquery.js" />
-$(document).ready(function () {
-    $('.custom-file-input').on("change", function () {
-        var fileName = $(this).val().split("\\").pop();
-        $(this).next('.custom-file-label').html(fileName);
-    });
-});
